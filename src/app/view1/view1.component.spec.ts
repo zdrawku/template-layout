a99a69b2cef052f2a35f041bd34006f25ebe7723
Zdravko Kolev <zdrawko.kolew@gmail.com>
2024-10-15 10:50:49 +0300

    untitled-app (22) updates

--- src/app/view1/view1.component.spec.ts
+++ src/app/view1/view1.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { IGX_BUTTON_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective, IGX_CARD_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent } from 'igniteui-angular';
 import { View1Component } from './view1.component';
 
 describe('View1Component', () => {
@@ -8,10 +11,12 @@ describe('View1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [View1Component]
+      imports: [ View1Component, NoopAnimationsModule, FormsModule, IGX_BUTTON_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective, IGX_CARD_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(View1Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
